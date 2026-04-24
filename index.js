9512d75a22d07f56273abf5345681744ee6a41ba
DhairyaSavita <savitadhairya2@gmail.com>
2026-04-24 11:13:07 +0530

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD + Monitoring 🚀");
+  res.send("CI/CD + Monitoring 🚀 Yes its working");
 });
 
 app.listen(3000, () => console.log("Server running"));
